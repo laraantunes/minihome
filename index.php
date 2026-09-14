79361fb4087456a88c96061bc0222d6db5892600
Lara <laraantunes01@outlook.com>
2026-09-14 13:32:28 -0300

    Fix on full screen behavior for some components

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <?php
-    $v = 21;
+    require_once __DIR__ . '/version.php';
+    $v = $version;
     ?>
     <!-- Main Styles -->
     <link rel="stylesheet" href="assets/css/style.css?v=<?= $v ?>">
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
 
-$version = '1.3.0';
+$version = '1.3.1';
